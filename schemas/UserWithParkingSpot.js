0ff1033d7162c854cd13f12019e0de8bf6cf30d9
Ata Hakkak <ata.hakkak@gmail.com>
2024-04-02 19:02:52 +0200

    added signup static method to UserWithParkingschema

--- schemas/UserWithParkingSpot.js
+++ schemas/UserWithParkingSpot.js
@@ -41,6 +41,40 @@ UserWithParkingSpotSchema.statics.login = async function (email, password) {
   return user;
 };
 
+// static custom signup method
+// PLEASE CHECK AND COMMENT IF !OK
+
+userSchema.static.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+};
+
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  if (!email || !password) {
+    throw Error("All fields are mandatory");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid email");
+  }
+  if (!validator.isStrongPassword) {
+    throw Error(
+      "Make sure to use at least 8 characters, one upper case letter, a number and a symbol"
+    );
+  }
+  const salt = await bcrypt.genSalt(10);
+
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
 module.exports = mongoose.model(
   "UserWithParkingSpot",
   UserWithParkingSpotSchema
